80b1e7a165e1ac35ffbaaaba4d861729ded539be
Daniel <daniel.kastenhofer1@gmail.com>
2020-10-07 16:55:08 +0200

    final commit

--- Maze.Robot/RobotController.cs
+++ Maze.Robot/RobotController.cs
@@ -37,9 +37,20 @@ namespace Maze.Solver
             var reachedEnd = false;
             robot.ReachedExit += (_, __) => reachedEnd = true;
 
+            Random rnd = new Random();
+            int moves = 0;
+
             while (!reachedEnd)
             {
-                robot.Move(Direction.Right);
+                robot.TryMove((Direction)rnd.Next(0, 4));
+
+                moves++;
+                if (moves == 10000000)
+                {
+                    robot.HaltAndCatchFire();
+                    break;
+                }
+
             }
         }
     }
